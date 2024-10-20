8d7d2a3096419a216b1bdab3ef09cf692d7bc8ec
hossenali2200 <hosenali2200@gmail.com>
2024-10-20 09:43:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+ 
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
